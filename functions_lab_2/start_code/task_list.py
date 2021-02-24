6848d705cbe4497626a6e221dba75436c37f0a1a
codeonium <codeon2020@gmail.com>
2021-02-24 21:00:37 +0000

    Up to exercise 7

--- functions_lab_2/start_code/task_list.py
+++ functions_lab_2/start_code/task_list.py
@@ -1,13 +1,3 @@
-# ### Learning Objectives
-# - Learn how to break down code into smaller chunks
-# - Learn how to use functions to encapsulate code
-# - Refactor the task list program to use functions
-
-# ### Brief
-
-# Given the following list of dictionaries, use functions throughout to create a program to manage a task list.
-
-
 tasks = [
     { "description": "Wash Dishes", "completed": False, "time_taken": 10 },
     { "description": "Clean Windows", "completed": False, "time_taken": 15 },
@@ -15,8 +5,6 @@ tasks = [
     { "description": "Feed Cat", "completed": False, "time_taken": 5 },
     { "description": "Walk Dog", "completed": True, "time_taken": 60 },
 ]
-
-# ### MVP
 
 # As a user, to manage my task list I would like a program that allows me to:
 
@@ -28,7 +16,7 @@ def uncomp_task(tasks_list):
             tasks_filtered.append(task["description"])
     return tasks_filtered
 tasks_not_completed = uncomp_task(tasks)
-print(f"This tasks have not been completed: {tasks_not_completed}")
+#print(f"This tasks have not been completed: {tasks_not_completed}")
 
 # 2. Print a list of completed tasks
 def comp_task(tasks_list):
@@ -38,7 +26,7 @@ def comp_task(tasks_list):
             tasks_filtered.append(task["description"])
     return tasks_filtered
 tasks_completed = comp_task(tasks)
-print(f"The following tasks are the reason you can have your feet up: {tasks_completed}")
+#print(f"The following tasks are the reason you can have your feet up: {tasks_completed}")
 
 # 3. Print a list of all task descriptions
 def all_tasks(tasks_list):
@@ -48,7 +36,7 @@ def all_tasks(tasks_list):
             tasks_filtered.append(task["description"])
     return tasks_filtered
 all_tasks_result = all_tasks(tasks)
-print(f"And here I present to you, all the tasks you had for today: {all_tasks_result}")
+#print(f"And here I present to you, all the tasks you had for today: {all_tasks_result}")
 
 # 4. Print a list of tasks where time_taken is at least a given time
 def time_it_takes(tasks_list):
@@ -58,7 +46,7 @@ def time_it_takes(tasks_list):
             tasks_filtered.append(task["description"])
     return tasks_filtered
 timed_task = time_it_takes(tasks)
-print(f"The following tasks all take more than 10 minutes: {timed_task}")
+#print(f"The following tasks all take more than 10 minutes: {timed_task}")
 
 # 5. Print any task with a given description
 def task_descript(tasks_list, task_description):
@@ -68,12 +56,24 @@ def task_descript(tasks_list, task_description):
             tasks_filtered.append(task["description"])
     return tasks_filtered
 tasks_result = task_descript(tasks, "Feed Cat")
-print(f"Hey, don't forget: {tasks_result}")
+#print(f"Hey, don't forget: {tasks_result}")
 
 # 6. Given a description update that task to mark it as complete.
+def task_now_completed(tasks_list, task_description):
+    tasks_filtered = []
+    for task in tasks_list:
+        if task["completed"] == False:
+            task["completed"] = True
+            return tasks_filtered
+tasks_now_result = task_now_completed(tasks, "feed the cat")
+print(tasks_now_result)
 
 # 7. Add a task to the list
-
+tasks.append({"description": "Relax",
+        "completed": False, 
+        "time_taken": 1 
+})
+#print(tasks)
 # ### Further Extensions
 
 # 8. Use a while loop to display the following menu and allow the use to enter an option.
